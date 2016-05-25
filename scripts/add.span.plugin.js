2344d3a627330d4b5b5b7fa2b82eb583e239503d
Kevin Brown <kevin_brown@tax.org>
2016-05-25 17:36:56 -0400

    adding minified version of plugin

--- scripts/add.span.plugin.js
+++ scripts/add.span.plugin.js
@@ -19,36 +19,10 @@ tinymce.PluginManager.add('example2', function (editor, url) {
 
 
 
-
-
-
-
-
-function highlight_tags() {
-    var html_output = tinyMCE.activeEditor.getContent();
-    html_output = highlight_remove(html_output);
-    var regex = new RegExp(/\[start\](((?!\[(?:end|start)\]).)+)\[end\]/ig);
-
-    html_output = html_output.replace(regex,'<span style="background-color:> yellow;">[start]$1[end]</span>');
-    tinyMCE.activeEditor.setContent(html_output);
-}
-
-function highlight_remove(html_output) {
-    var regex_fix = new RegExp(/<span\sstyle="background-color:\syellow;">(.+?)<\/span>/ig);
-    return html_output.replace(regex_fix,'$1');
-}
-
-
 function add_spans_to_sections(html_output) {
     var replacement_pre = '<span class="r_entity r_codesection" typeof="tae:CodeSection"><span class="r_prop r_name" property="schema:name">';
     var replacement_post = '</span></span>';
-    var regex ='/(\d+\S*)(\([\w\(\)]+\))';
-
-    //var regex_fix = new RegExp(/\d+\S*(\([\w\(\)]+\)));
-   txt = html_output.replace(/\d+\S*(\([\w\(\)]+\))/mg,replacement_pre+"\$&"+replacement_post); // $& means the whole matched string
-
-    //txt = html_output.replace(regex_fix,replacement_pre+'$1'+replacement_post);
-
+    txt = html_output.replace(/\d+\S*(\([\w\(\)]+\))/mg,replacement_pre+"\$&"+replacement_post); // $& means the whole matched string
     return txt;
 }
 
--- scripts/add.testing.text.plugin.js
+++ scripts/add.testing.text.plugin.js
@@ -22,20 +22,6 @@ tinymce.PluginManager.add('testingtext', function (editor, url) {
 
 
 
-function highlight_tags() {
-    var html_output = tinyMCE.activeEditor.getContent();
-    html_output = highlight_remove(html_output);
-    var regex = new RegExp(/\[start\](((?!\[(?:end|start)\]).)+)\[end\]/ig);
-
-    html_output = html_output.replace(regex,'<span style="background-color:> yellow;">[start]$1[end]</span>');
-    tinyMCE.activeEditor.setContent(html_output);
-}
-
-function highlight_remove(html_output) {
-    var regex_fix = new RegExp(/<span\sstyle="background-color:\syellow;">(.+?)<\/span>/ig);
-    return html_output.replace(regex_fix,'$1');
-}
-
 function get_testing_text(){
     var htmlString = `Amendment:
 Section 106(a) of title I of div. B of Pub. L. 110-343 (Energy Improvement and Extension Act of 2008) amends Code section 25D(g) by striking "December 31, 2008" and inserting "December 31, 2016".
